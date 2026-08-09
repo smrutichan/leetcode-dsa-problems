0ec551bc292852e2d40203db7d64ac502290ac67
smruti <csmruti30@gmail.com>
2026-08-09 09:40:04 +0530

    Update  01_Two_Sum.py

---
+++ Easy/Arrays_Strings/01_Two_Sum.py
@@ -0,0 +1,19 @@
+'''
+Data Structure Used: unordered_map
+Time Complexity: O(n)
+Space Complexity: O(1) 
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in d:
+                return [d[complement],i]
+            
+            d[nums[i]] = i
+            
+        return result
